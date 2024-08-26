80342ab135a8a07afb5e046fc1680429f2342648
wangtao3 <wangtao3@izuche.com>
2024-08-26 14:05:35 +0800

    111

--- Dockerfile
+++ Dockerfile
@@ -17,3 +17,5 @@ FROM nginx:latest
 COPY nginx/default.conf /etc/nginx/conf.d/default.conf
 COPY --from=builder /app/dist  /usr/share/nginx/html
 #从之前命名为 builder 的阶段中复制 /app/dist 目录下的文件到最终镜像中的 Nginx 默认 HTML 目录中，用于提供静态文件服务。
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(), // hash模式:createWebHashHistory，history模式:createWebHistory
+  history: createWebHashHistory(), // hash模式:createWebHashHistory，history模式:createWebHistory
   routes: [
     {
       path: "/",
